74a61201f02ce60555c58a42f18310161bd5ca58
Roberto Abrão Pereira <robertoabraopereira@gmail.com>
2025-07-30 17:23:20 +0000

    o problema do gymvisual.com é que tudo fica fica com "?"

--- src/lib/exercise-gifs.ts
+++ src/lib/exercise-gifs.ts
@@ -1,62 +1,62 @@
 
 export const exerciseGifMap: Record<string, string> = {
   // --- PEITORAL ---
-  "Supino Reto (Barra ou Halteres)": "https://gymvisual.com/img/p/1/7/4/3/9/17439.gif", // Barbell Bench Press
-  "Supino Inclinado (Barra ou Halteres)": "https://gymvisual.com/img/p/1/7/4/2/9/17429.gif", // Barbell Incline Bench Press
-  "Supino Declinado": "https://gymvisual.com/img/p/1/7/2/5/7/17257.gif", // Dumbbell Decline Bench Press
-  "Crucifixo (Halteres ou Máquina)": "https://gymvisual.com/img/p/1/7/2/6/8/17268.gif", // Dumbbell Fly
-  "Voador Peitoral": "https://gymvisual.com/img/p/1/7/5/4/1/17541.gif", // Lever Pec Deck Fly
-  "Supino na Máquina": "https://gymvisual.com/img/p/1/7/5/2/9/17529.gif", // Lever Chest Press
+  "Supino Reto (Barra ou Halteres)": "https://musclewiki.com/media/uploads/male-barbell-bench-press.gif",
+  "Supino Inclinado (Barra ou Halteres)": "https://musclewiki.com/media/uploads/male-barbell-incline-bench-press.gif",
+  "Supino Declinado": "https://musclewiki.com/media/uploads/male-barbell-decline-bench-press.gif",
+  "Crucifixo (Halteres ou Máquina)": "https://musclewiki.com/media/uploads/male-dumbbell-fly.gif",
+  "Voador Peitoral": "https://musclewiki.com/media/uploads/male-machine-fly-pec-deck.gif",
+  "Supino na Máquina": "https://musclewiki.com/media/uploads/male-machine-chest-press.gif",
 
   // --- COSTAS ---
-  "Puxada Frontal": "https://gymvisual.com/img/p/1/7/2/2/8/17228.gif", // Cable Lat Pulldown
-  "Remada Curvada": "https://gymvisual.com/img/p/1/7/4/5/1/17451.gif", // Barbell Bent Over Row
-  "Remada Cavalinho": "https://gymvisual.com/img/p/1/7/3/9/5/17395.gif", // T-bar Row
-  "Remada Serrote": "https://gymvisual.com/img/p/1/7/2/9/8/17298.gif", // Dumbbell Bent Over Row
-  "Levantamento Terra": "https://gymvisual.com/img/p/1/7/4/3/6/17436.gif", // Barbell Deadlift
-  "Puxada Alta": "https://gymvisual.com/img/p/1/7/4/8/2/17482.gif", // Cable Straight Arm Pulldown
-  "Remada na Máquina": "https://gymvisual.com/img/p/1/7/3/2/4/17324.gif", // Lever Seated Row
+  "Puxada Frontal": "https://musclewiki.com/media/uploads/male-cable-lat-pulldown.gif",
+  "Remada Curvada": "https://musclewiki.com/media/uploads/male-barbell-bent-over-row.gif",
+  "Remada Cavalinho": "https://musclewiki.com/media/uploads/male-machine-t-bar-row.gif",
+  "Remada Serrote": "https://musclewiki.com/media/uploads/male-dumbbell-one-arm-row.gif",
+  "Levantamento Terra": "https://musclewiki.com/media/uploads/male-barbell-deadlift.gif",
+  "Puxada Alta": "https://musclewiki.com/media/uploads/male-cable-straight-arm-pulldown.gif",
+  "Remada na Máquina": "https://musclewiki.com/media/uploads/male-machine-seated-row.gif",
 
   // --- OMBROS ---
-  "Desenvolvimento de Ombros (Barra ou Halteres)": "https://gymvisual.com/img/p/1/7/2/9/5/17295.gif", // Dumbbell Shoulder Press
-  "Desenvolvimento na Máquina": "https://gymvisual.com/img/p/1/7/5/4/4/17544.gif", // Machine Shoulder Press
-  "Elevação Lateral": "https://gymvisual.com/img/p/1/7/2/6/5/17265.gif", // Dumbbell Lateral Raise
-  "Remada Alta": "https://gymvisual.com/img/p/1/7/4/4/9/17449.gif", // Barbell Upright Row
-  "Encolhimento de Ombros": "https://gymvisual.com/img/p/1/7/2/5/9/17259.gif", // Dumbbell Shrug
+  "Desenvolvimento de Ombros (Barra ou Halteres)": "https://musclewiki.com/media/uploads/male-dumbbell-shoulder-press.gif",
+  "Desenvolvimento na Máquina": "https://musclewiki.com/media/uploads/male-machine-shoulder-press.gif",
+  "Elevação Lateral": "https://musclewiki.com/media/uploads/male-dumbbell-lateral-raise.gif",
+  "Remada Alta": "https://musclewiki.com/media/uploads/male-barbell-upright-row.gif",
+  "Encolhimento de Ombros": "https://musclewiki.com/media/uploads/male-dumbbell-shrug.gif",
 
   // --- BÍCEPS ---
-  "Rosca Direta (Barra ou Halteres)": "https://gymvisual.com/img/p/1/7/4/5/6/17456.gif", // Barbell Curl
-  "Rosca Martelo": "https://gymvisual.com/img/p/1/7/2/8/0/17280.gif", // Dumbbell Hammer Curl
-  "Rosca Alternada": "https://gymvisual.com/img/p/1/7/2/5/3/17253.gif", // Dumbbell Curl
-  "Rosca Concentrada": "https://gymvisual.com/img/p/1/7/2/7/2/17272.gif", // Dumbbell Concentration Curl
-  "Rosca Inversa": "https://gymvisual.com/img/p/1/7/4/4/5/17445.gif", // Barbell Reverse Curl
-  "Rosca 21": "https://gymvisual.com/img/p/1/7/4/5/6/17456.gif", // Representado pelo Rosca Direta
+  "Rosca Direta (Barra ou Halteres)": "https://musclewiki.com/media/uploads/male-barbell-curl.gif",
+  "Rosca Martelo": "https://musclewiki.com/media/uploads/male-dumbbell-hammer-curl.gif",
+  "Rosca Alternada": "https://musclewiki.com/media/uploads/male-dumbbell-curl.gif",
+  "Rosca Concentrada": "https://musclewiki.com/media/uploads/male-dumbbell-concentration-curl.gif",
+  "Rosca Inversa": "https://musclewiki.com/media/uploads/male-barbell-reverse-curl.gif",
+  "Rosca 21": "https://musclewiki.com/media/uploads/male-barbell-curl.gif", 
 
   // --- TRÍCEPS ---
-  "Tríceps na Polia": "https://gymvisual.com/img/p/1/7/4/7/9/17479.gif", // Cable Triceps Pushdown
-  "Tríceps Testa": "https://gymvisual.com/img/p/1/7/4/3/2/17432.gif", // Barbell Lying Triceps Extension
-  "Tríceps Francês": "https://gymvisual.com/img/p/1/7/2/8/6/17286.gif", // Dumbbell Lying Triceps Extension
-  "Tríceps Coice": "https://gymvisual.com/img/p/1/7/2/8/4/17284.gif", // Dumbbell Kickback
+  "Tríceps na Polia": "https://musclewiki.com/media/uploads/male-cable-triceps-pushdown.gif",
+  "Tríceps Testa": "https://musclewiki.com/media/uploads/male-barbell-lying-triceps-extension.gif",
+  "Tríceps Francês": "https://musclewiki.com/media/uploads/male-dumbbell-lying-triceps-extension.gif",
+  "Tríceps Coice": "https://musclewiki.com/media/uploads/male-dumbbell-kickback.gif",
 
   // --- PERNAS ---
-  "Agachamento Livre": "https://gymvisual.com/img/p/1/7/4/4/8/17448.gif", // Barbell Squat
-  "Leg Press": "https://gymvisual.com/img/p/1/7/3/8/5/17385.gif", // Sled Leg Press
-  "Cadeira Extensora": "https://gymvisual.com/img/p/1/7/5/3/5/17535.gif", // Lever Leg Extension
-  "Cadeira Flexora": "https://gymvisual.com/img/p/1/7/3/2/1/17321.gif", // Lever Seated Leg Curl
-  "Mesa Flexora": "https://gymvisual.com/img/p/1/7/3/2/6/17326.gif", // Lever Lying Leg Curl
-  "Stiff com Barra": "https://gymvisual.com/img/p/1/7/4/5/0/17450.gif", // Barbell Stiff Leg Deadlift
-  "Avanço com Halteres": "https://gymvisual.com/img/p/1/7/2/8/8/17288.gif", // Dumbbell Lunge
-  "Elevação Pélvica": "https://gymvisual.com/img/p/1/7/2/1/4/17214.gif", // Barbell Hip Thrust
-  "Agachamento Goblet": "https://gymvisual.com/img/p/1/7/5/1/1/17511.gif", // Goblet Squat
+  "Agachamento Livre": "https://musclewiki.com/media/uploads/male-barbell-squat.gif",
+  "Leg Press": "https://musclewiki.com/media/uploads/male-machine-leg-press.gif",
+  "Cadeira Extensora": "https://musclewiki.com/media/uploads/male-machine-leg-extension.gif",
+  "Cadeira Flexora": "https://musclewiki.com/media/uploads/male-machine-seated-leg-curl.gif",
+  "Mesa Flexora": "https://musclewiki.com/media/uploads/male-machine-lying-leg-curl.gif",
+  "Stiff com Barra": "https://musclewiki.com/media/uploads/male-barbell-stiff-leg-deadlift.gif",
+  "Avanço com Halteres": "https://musclewiki.com/media/uploads/male-dumbbell-lunges.gif",
+  "Elevação Pélvica": "https://musclewiki.com/media/uploads/male-barbell-hip-thrust.gif",
+  "Agachamento Goblet": "https://musclewiki.com/media/uploads/male-dumbbell-goblet-squat.gif",
 
   // --- PANTURRILHAS ---
-  "Panturrilha em Pé": "https://gymvisual.com/img/p/1/7/5/5/2/17552.gif", // Lever Standing Calf Raise
-  "Panturrilha Sentado": "https://gymvisual.com/img/p/1/7/3/8/8/17388.gif", // Sled Seated Calf Raise
+  "Panturrilha em Pé": "https://musclewiki.com/media/uploads/male-machine-standing-calf-raise.gif",
+  "Panturrilha Sentado": "https://musclewiki.com/media/uploads/male-machine-seated-calf-raise.gif",
 
   // --- ABDÔMEN & CORE ---
-  "Prancha Isométrica": "https://gymvisual.com/img/p/1/7/3/5/4/17354.gif", // Plank
-  "Abdominal Remador": "https://gymvisual.com/img/p/1/7/4/0/2/17402.gif", // V-up
+  "Prancha Isométrica": "https://musclewiki.com/media/uploads/male-bodyweight-plank.gif",
+  "Abdominal Remador": "https://musclewiki.com/media/uploads/male-bodyweight-v-ups.gif",
 
   // --- CARDIO ---
-  "Cardio (Corrida, Bicicleta, Elíptico)": "https://gymvisual.com/img/p/1/7/3/9/7/17397.gif", // Treadmill
+  "Cardio (Corrida, Bicicleta, Elíptico)": "https://musclewiki.com/media/uploads/male-machine-treadmill.gif",
 };
