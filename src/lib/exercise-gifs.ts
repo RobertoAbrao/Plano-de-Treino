2429186fe19b66bdef15a527230397e67b5ab929
Roberto Abrão Pereira <robertoabraopereira@gmail.com>
2025-07-30 17:21:50 +0000

    não gostei, vamos procurar outra alternativa

--- src/lib/exercise-gifs.ts
+++ src/lib/exercise-gifs.ts
@@ -1,46 +1,62 @@
 
 export const exerciseGifMap: Record<string, string> = {
-  // Plano inicial - Mapeado para exrx.net
-  "Supino Reto (Barra ou Halteres)": "https://exrx.net/file/clip/PectoralSternal/BarbellBenchPress.gif",
-  "Supino Inclinado (Barra ou Halteres)": "https://exrx.net/file/clip/PectoralClavicular/InclineDumbbellPress.gif",
-  "Desenvolvimento na Máquina": "https://exrx.net/file/clip/DeltoidAnterior/LeverShoulderPress.gif",
-  "Elevação Lateral": "https://exrx.net/file/clip/DeltoidLateral/DumbbellLateralRaise.gif",
-  "Puxada Frontal": "https://exrx.net/file/clip/LatissimusDorsi/CBFrontPulldown.gif",
-  "Remada Curvada": "https://exrx.net/file/clip/BackGeneral/BentOverBarbellRow.gif",
-  "Rosca Direta (Barra ou Halteres)": "https://exrx.net/file/clip/Biceps/BarbellCurl.gif",
-  "Rosca Martelo": "https://exrx.net/file/clip/Brachialis/DumbbellHammerCurl.gif",
-  "Agachamento Livre": "https://exrx.net/file/clip/Quadriceps/BarbellSquat.gif",
-  "Leg Press": "https://exrx.net/file/clip/Quadriceps/Sled45LegPress.gif",
-  "Cadeira Extensora": "https://exrx.net/file/clip/Quadriceps/LeverLegExtension.gif",
-  "Mesa Flexora": "https://exrx.net/file/clip/Hamstrings/LeverLyingLegCurl.gif",
-  "Panturrilha em Pé": "https://exrx.net/file/clip/Gastrocnemius/LeverStandingCalfRaise.gif",
-  "Prancha Isométrica": "https://exrx.net/file/clip/RectusAbdominis/FrontPlank.gif",
-  "Abdominal Remador": "https://exrx.net/file/clip/HipFlexors/VUp.gif",
-  "Levantamento Terra": "https://exrx.net/file/clip/GluteusMaximus/BarbellDeadlift.gif",
-  "Remada Cavalinho": "https://exrx.net/file/clip/BackGeneral/LeverT-barRow.gif",
-  "Elevação Pélvica": "https://exrx.net/file/clip/GluteusMaximus/BarbellHipThrust.gif",
+  // --- PEITORAL ---
+  "Supino Reto (Barra ou Halteres)": "https://gymvisual.com/img/p/1/7/4/3/9/17439.gif", // Barbell Bench Press
+  "Supino Inclinado (Barra ou Halteres)": "https://gymvisual.com/img/p/1/7/4/2/9/17429.gif", // Barbell Incline Bench Press
+  "Supino Declinado": "https://gymvisual.com/img/p/1/7/2/5/7/17257.gif", // Dumbbell Decline Bench Press
+  "Crucifixo (Halteres ou Máquina)": "https://gymvisual.com/img/p/1/7/2/6/8/17268.gif", // Dumbbell Fly
+  "Voador Peitoral": "https://gymvisual.com/img/p/1/7/5/4/1/17541.gif", // Lever Pec Deck Fly
+  "Supino na Máquina": "https://gymvisual.com/img/p/1/7/5/2/9/17529.gif", // Lever Chest Press
 
-  // Fallbacks em inglês para outros exercícios (se necessário)
-  "barbell bench press": "https://exrx.net/file/clip/PectoralSternal/BarbellBenchPress.gif",
-  "dumbbell bench press": "https://exrx.net/file/clip/PectoralSternal/DumbbellBenchPress.gif",
-  "incline dumbbell press": "https://exrx.net/file/clip/PectoralClavicular/InclineDumbbellPress.gif",
-  "dumbbell flye": "https://exrx.net/file/clip/PectoralSternal/DumbbellFly.gif",
-  "dumbbell shoulder press": "https://exrx.net/file/clip/DeltoidAnterior/DumbbellShoulderPress.gif",
-  "dumbbell lateral raise": "https://exrx.net/file/clip/DeltoidLateral/DumbbellLateralRaise.gif",
-  "bent over row": "https://exrx.net/file/clip/BackGeneral/BentOverBarbellRow.gif",
-  "dumbbell row": "https://exrx.net/file/clip/BackGeneral/DumbbellRow.gif",
-  "lat pulldown": "https://exrx.net/file/clip/LatissimusDorsi/CBFrontPulldown.gif",
-  "barbell curl": "https://exrx.net/file/clip/Biceps/BarbellCurl.gif",
-  "hammer curl": "https://exrx.net/file/clip/Brachialis/DumbbellHammerCurl.gif",
-  "triceps pushdown": "https://exrx.net/file/clip/Triceps/CBPushdown.gif",
-  "lying triceps extension": "https://exrx.net/file/clip/Triceps/BarbellLyingTricepsExtension.gif",
-  "dumbbell kickback": "https://exrx.net/file/clip/Triceps/DBKickback.gif",
-  "squat": "https://exrx.net/file/clip/Quadriceps/BarbellSquat.gif",
-  "lunge": "https://exrx.net/file/clip/Quadriceps/DumbbellLunge.gif",
-  "leg press": "https://exrx.net/file/clip/Quadriceps/Sled45LegPress.gif",
-  "leg extension": "https://exrx.net/file/clip/Quadriceps/LeverLegExtension.gif",
-  "lying leg curl": "https://exrx.net/file/clip/Hamstrings/LeverLyingLegCurl.gif",
-  "deadlift": "https://exrx.net/file/clip/GluteusMaximus/BarbellDeadlift.gif",
-  "plank": "https://exrx.net/file/clip/RectusAbdominis/FrontPlank.gif",
-  "crunches": "https://exrx.net/file/clip/RectusAbdominis/Crunch.gif"
+  // --- COSTAS ---
+  "Puxada Frontal": "https://gymvisual.com/img/p/1/7/2/2/8/17228.gif", // Cable Lat Pulldown
+  "Remada Curvada": "https://gymvisual.com/img/p/1/7/4/5/1/17451.gif", // Barbell Bent Over Row
+  "Remada Cavalinho": "https://gymvisual.com/img/p/1/7/3/9/5/17395.gif", // T-bar Row
+  "Remada Serrote": "https://gymvisual.com/img/p/1/7/2/9/8/17298.gif", // Dumbbell Bent Over Row
+  "Levantamento Terra": "https://gymvisual.com/img/p/1/7/4/3/6/17436.gif", // Barbell Deadlift
+  "Puxada Alta": "https://gymvisual.com/img/p/1/7/4/8/2/17482.gif", // Cable Straight Arm Pulldown
+  "Remada na Máquina": "https://gymvisual.com/img/p/1/7/3/2/4/17324.gif", // Lever Seated Row
+
+  // --- OMBROS ---
+  "Desenvolvimento de Ombros (Barra ou Halteres)": "https://gymvisual.com/img/p/1/7/2/9/5/17295.gif", // Dumbbell Shoulder Press
+  "Desenvolvimento na Máquina": "https://gymvisual.com/img/p/1/7/5/4/4/17544.gif", // Machine Shoulder Press
+  "Elevação Lateral": "https://gymvisual.com/img/p/1/7/2/6/5/17265.gif", // Dumbbell Lateral Raise
+  "Remada Alta": "https://gymvisual.com/img/p/1/7/4/4/9/17449.gif", // Barbell Upright Row
+  "Encolhimento de Ombros": "https://gymvisual.com/img/p/1/7/2/5/9/17259.gif", // Dumbbell Shrug
+
+  // --- BÍCEPS ---
+  "Rosca Direta (Barra ou Halteres)": "https://gymvisual.com/img/p/1/7/4/5/6/17456.gif", // Barbell Curl
+  "Rosca Martelo": "https://gymvisual.com/img/p/1/7/2/8/0/17280.gif", // Dumbbell Hammer Curl
+  "Rosca Alternada": "https://gymvisual.com/img/p/1/7/2/5/3/17253.gif", // Dumbbell Curl
+  "Rosca Concentrada": "https://gymvisual.com/img/p/1/7/2/7/2/17272.gif", // Dumbbell Concentration Curl
+  "Rosca Inversa": "https://gymvisual.com/img/p/1/7/4/4/5/17445.gif", // Barbell Reverse Curl
+  "Rosca 21": "https://gymvisual.com/img/p/1/7/4/5/6/17456.gif", // Representado pelo Rosca Direta
+
+  // --- TRÍCEPS ---
+  "Tríceps na Polia": "https://gymvisual.com/img/p/1/7/4/7/9/17479.gif", // Cable Triceps Pushdown
+  "Tríceps Testa": "https://gymvisual.com/img/p/1/7/4/3/2/17432.gif", // Barbell Lying Triceps Extension
+  "Tríceps Francês": "https://gymvisual.com/img/p/1/7/2/8/6/17286.gif", // Dumbbell Lying Triceps Extension
+  "Tríceps Coice": "https://gymvisual.com/img/p/1/7/2/8/4/17284.gif", // Dumbbell Kickback
+
+  // --- PERNAS ---
+  "Agachamento Livre": "https://gymvisual.com/img/p/1/7/4/4/8/17448.gif", // Barbell Squat
+  "Leg Press": "https://gymvisual.com/img/p/1/7/3/8/5/17385.gif", // Sled Leg Press
+  "Cadeira Extensora": "https://gymvisual.com/img/p/1/7/5/3/5/17535.gif", // Lever Leg Extension
+  "Cadeira Flexora": "https://gymvisual.com/img/p/1/7/3/2/1/17321.gif", // Lever Seated Leg Curl
+  "Mesa Flexora": "https://gymvisual.com/img/p/1/7/3/2/6/17326.gif", // Lever Lying Leg Curl
+  "Stiff com Barra": "https://gymvisual.com/img/p/1/7/4/5/0/17450.gif", // Barbell Stiff Leg Deadlift
+  "Avanço com Halteres": "https://gymvisual.com/img/p/1/7/2/8/8/17288.gif", // Dumbbell Lunge
+  "Elevação Pélvica": "https://gymvisual.com/img/p/1/7/2/1/4/17214.gif", // Barbell Hip Thrust
+  "Agachamento Goblet": "https://gymvisual.com/img/p/1/7/5/1/1/17511.gif", // Goblet Squat
+
+  // --- PANTURRILHAS ---
+  "Panturrilha em Pé": "https://gymvisual.com/img/p/1/7/5/5/2/17552.gif", // Lever Standing Calf Raise
+  "Panturrilha Sentado": "https://gymvisual.com/img/p/1/7/3/8/8/17388.gif", // Sled Seated Calf Raise
+
+  // --- ABDÔMEN & CORE ---
+  "Prancha Isométrica": "https://gymvisual.com/img/p/1/7/3/5/4/17354.gif", // Plank
+  "Abdominal Remador": "https://gymvisual.com/img/p/1/7/4/0/2/17402.gif", // V-up
+
+  // --- CARDIO ---
+  "Cardio (Corrida, Bicicleta, Elíptico)": "https://gymvisual.com/img/p/1/7/3/9/7/17397.gif", // Treadmill
 };
