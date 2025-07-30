f8a7ad3389fc57bad34fa20192d9edfd3ada69b0
Roberto Abrão Pereira <robertoabraopereira@gmail.com>
2025-07-30 14:48:50 +0000

    descobri o problema, o site já está em portugues, mas ele insiste em tra

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="pt-BR" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
